010df465b085750238169acd028592daa644d234
mvill <119754846+migavg@users.noreply.github.com>
2023-02-26 13:16:22 -0700

    started a basic write function on line 56 in order to log errors or success

--- Develop/index.js
+++ Develop/index.js
@@ -54,7 +54,9 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("README.md",)
+    fs.writeFile(fileName, data, (err) => 
+   err ? console.log(err) : console.log("Successfully created README.md")
+    );
 }
 
 // TODO: Create a function to initialize app
