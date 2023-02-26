e589c593d777963c7bba7cd2591427d8d6aca7d9
mvill <119754846+migavg@users.noreply.github.com>
2023-02-25 23:23:58 -0700

    added questions to the const questions array

--- Develop/index.js
+++ Develop/index.js
@@ -2,10 +2,60 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name: "title",
+        message: "What is your title?",
+    },
+
+    {
+        type:"input",
+        name: "description",
+        message: "What is your description?",
+    },
+
+    {
+        type:"input",
+        name: "installation",
+        message: "How do you install?",
+    },
+
+    {
+        type:"input",
+        name: "usage",
+        message: "What is your usage of this project?",
+    },
+
+    {
+        type:"input",
+        name: "contribution",
+        message: "What are the contribution guidelines?",
+    },
+
+   {
+        type:"input",
+        name: "test",
+        message: "What are the test instructions?",
+    },
+
+    {
+        type:"input",
+        name: "github",
+        message: "What is your GitHub?",
+    },
+
+    {
+        type:"input",
+        name: "email",
+        message: "What is your email?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md",)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
